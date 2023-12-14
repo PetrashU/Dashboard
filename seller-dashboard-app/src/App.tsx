6280fa174df10e4c78b020099da4ae92ae387345
bilskik <kbilski77@gmail.com>
2023-12-14 20:48:34 +0100

    Adding simple routing, adding some basic styles for login cmpt

--- seller-dashboard-app/src/App.tsx
+++ seller-dashboard-app/src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import Login from './page/Login';
+import Login from './page/login/Login';
+import { BrowserRouter, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Dashboard from './page/dashboard/Dashboard';
 
 function App() {
+    const router = createBrowserRouter([
+      {
+        path: "/",
+        element : <Login/>
+      },
+      {
+      path : "/home",
+      element: <Dashboard/>
+      }
+    ])
+
     return (
-      <>
+      <BrowserRouter>
         <Login/>
-      </>
+      </BrowserRouter>
     );
 }
 
