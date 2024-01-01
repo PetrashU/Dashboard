cd4f0542ebfdc006ccae82ed3b5323088a564359
Ulyana Petrashevich <petrashevichulyana@gmail.com>
2024-01-01 19:19:43 +0100

    Orders widget without redirection

--- seller-dashboard-app/src/component/widget/OrdersWidg.tsx
+++ seller-dashboard-app/src/component/widget/OrdersWidg.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/store/store';
+import { ordersStats } from '../../data/fakedata';
 import './widgets.css';
 
 const OrdersWidg = () => {
@@ -9,11 +10,18 @@ const OrdersWidg = () => {
     return state.language.currentLanguage;
   })
 
-  const ordersData = [
-    { id: 1, category: "Unpaid", count: 10 },
-    { id: 2, category: "Not Mailed", count: 5 },
-    { id: 3, category: "Returned", count: 3 },
-  ];
+  const translate = (category: string) => {
+    switch (category) {
+      case 'UNPAID':
+        return 'NIEOPŁACONE';
+      case 'NOT MAILED':
+        return 'NIEWYSŁANE';
+      case 'RETURNED':
+        return 'ZWRÓCONE';
+      default:
+        return category;
+    }
+  };
 
   return (
     <div className="orders-widget">
@@ -24,14 +32,14 @@ const OrdersWidg = () => {
         <table className='orders-table'>
           <thead>
             <tr>
-              <th>{language === 'English' ? 'CATEGORIE' : 'KATEGORIA'}</th>
+              <th>{language === 'English' ? 'CATEGORY' : 'KATEGORIA'}</th>
               <th>{language === 'English' ? 'NUMBER OF ORDERS' : 'LICZBA ZAMÓWIEŃ'}</th>
             </tr>
           </thead>
           <tbody>
-            {ordersData.map(order => (
+            {ordersStats.map(order => (
               <tr key={order.id}>
-                <td>{order.category}</td>
+                <td>{language === 'English' ? order.category : translate(order.category)}</td>
                 <td>{order.count}</td>
               </tr>
             ))}
--- seller-dashboard-app/src/data/fakedata.ts
+++ seller-dashboard-app/src/data/fakedata.ts
@@ -1,99 +1,122 @@
 export type Review = {
-    id: number;
-    language: string;
-    product: string;
-    text: string;
-    stars: number;
+  id: number;
+  language: string;
+  product: string;
+  text: string;
+  stars: number;
 };
 
 export const reviews: Review[] = [
-    {
-        id: 1,
-        language: "Polish",
-        product: "Statyw",
-        text: "Doskonała stabilność i lekkość. Idealny do fotografii podróżniczej!",
-        stars: 5
-    },
-    {
-        id: 2,
-        language: "English",
-        product: "Camera Bag",
-        text: "Spacious but the material feels a bit cheap.",
-        stars: 3
-    },
-    {
-        id: 3,
-        language: "Polish",
-        product: "Karta pamięci",
-        text: "Nie tak szybka jak zapowiadano, dość rozczarowująca.",
-        stars: 2
-    },
-    {
-        id: 4,
-        language: "Polish",
-        product: "Filtr obiektywu",
-        text: "Doskonała przejrzystość i trwałość. Niezbędny do ochrony obiektywu.",
-        stars: 5
-    },
-    {
-        id: 5,
-        language: "English",
-        product: "Flash",
-        text: "Overheats quickly and battery life is poor.",
-        stars: 1
-    },
-    {
-        id: 6,
-        language: "Polish",
-        product: "Karta pamięci",
-        text: "Dobra dla swojej ceny",
-        stars: 4
-    },
+  {
+    id: 1,
+    language: "Polish",
+    product: "Statyw",
+    text: "Doskonała stabilność i lekkość. Idealny do fotografii podróżniczej!",
+    stars: 5
+  },
+  {
+    id: 2,
+    language: "English",
+    product: "Camera Bag",
+    text: "Spacious but the material feels a bit cheap.",
+    stars: 3
+  },
+  {
+    id: 3,
+    language: "Polish",
+    product: "Karta pamięci",
+    text: "Nie tak szybka jak zapowiadano, dość rozczarowująca.",
+    stars: 2
+  },
+  {
+    id: 4,
+    language: "Polish",
+    product: "Filtr obiektywu",
+    text: "Doskonała przejrzystość i trwałość. Niezbędny do ochrony obiektywu.",
+    stars: 5
+  },
+  {
+    id: 5,
+    language: "English",
+    product: "Flash",
+    text: "Overheats quickly and battery life is poor.",
+    stars: 1
+  },
+  {
+    id: 6,
+    language: "Polish",
+    product: "Karta pamięci",
+    text: "Dobra dla swojej ceny",
+    stars: 4
+  },
 ];
 
 export type ProductSale = {
-    id: number;
-    productName: string;
-    salesNumber: number;
-    revenue: number;
-  };
-  
-  export const productSalesData: ProductSale[] = [
-    {
-      id: 1,
-      productName: "Statyw Fotograficzny",
-      salesNumber: 15,
-      revenue: 450
-    },
-    {
-      id: 2,
-      productName: "Aparat Cyfrowy",
-      salesNumber: 6,
-      revenue: 24000
-    },
-    {
-      id: 3,
-      productName: "Obiektyw 50mm",
-      salesNumber: 4,
-      revenue: 12000
-    },
-    {
-      id: 4,
-      productName: "Torba na Aparat",
-      salesNumber: 13,
-      revenue: 7000
-    },
-    {
-      id: 5,
-      productName: "Mikrofon Kierunkowy",
-      salesNumber: 8,
-      revenue: 5000
-    },
-    {
-      id: 6,
-      productName: "Oświetlenie LED",
-      salesNumber: 10,
-      revenue: 400
-    }
-  ];
-  
+  id: number;
+  productName: string;
+  salesNumber: number;
+  revenue: number;
+};
+
+export const productSalesData: ProductSale[] = [
+  {
+    id: 1,
+    productName: "Statyw Fotograficzny",
+    salesNumber: 15,
+    revenue: 450
+  },
+  {
+    id: 2,
+    productName: "Aparat Cyfrowy",
+    salesNumber: 6,
+    revenue: 24000
+  },
+  {
+    id: 3,
+    productName: "Obiektyw 50mm",
+    salesNumber: 4,
+    revenue: 12000
+  },
+  {
+    id: 4,
+    productName: "Torba na Aparat",
+    salesNumber: 13,
+    revenue: 7000
+  },
+  {
+    id: 5,
+    productName: "Mikrofon Kierunkowy",
+    salesNumber: 8,
+    revenue: 5000
+  },
+  {
+    id: 6,
+    productName: "Oświetlenie LED",
+    salesNumber: 10,
+    revenue: 400
+  }
+];
+
+export type OrdersStat = {
+  id: number;
+  category: string;
+  count: number;
+};
+
+export const ordersStats: OrdersStat[] = [
+  { 
+    id: 1, 
+    category: "UNPAID", 
+    count: 10 
+  },
+  { 
+    id: 2, 
+    category: "NOT MAILED", 
+    count: 5 
+  },
+  {
+    id: 3,
+    category: "RETURNED", 
+    count: 3 
+  },
+];
